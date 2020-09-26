7f89dff79cbe957ad6b94975cc60fafc26d643e4
vincent98-creator <vchidzwanda@gmail.com>
2020-09-26 23:17:42 +0200

    merchant view

--- src/app/views/base/services/merchant.service.ts
+++ src/app/views/base/services/merchant.service.ts
@@ -1,19 +1,37 @@
-import { Post } from './../model/Post';
-import { HttpClient } from '@angular/common/http';
+import { tap,map } from 'rxjs/operators';
+
+
+
+import { HttpClient, HttpHeaders ,HttpClientModule} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Merchant} from "../model/Merchant";
+const httpOptions ={
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
 export class MerchantService {
 
-base ="https://jsonplaceholder.typicode.com/posts";
+
+merchants:Merchant[]
+merchant:Merchant;
+//merchants:Merchant[];
+
+
+
+base ="http://localhost:8080/merchant/delete";
 baseU='http://localhost:8080/merchant/findAll';
   constructor(private http: HttpClient) { }
 
-  getMerchant():Observable<Post[]>{
-    return this.http.get<Post[]>(this.baseU);
+  getMerchant():Observable<Merchant[]>{
+    return this.http.get<Merchant[]>(this.baseU)
+    .pipe(map(response=>response)
+    ,tap(merchants=>console.log("Merchants",this.merchants)));
   }
   createMerchant(user){
     return this.http.post('http://localhost:8080/merchant/register', user);
